8f2724b1f2e25aaf86f2569208ddfa20d8a25942
Bradley <xxbradley.owensxx@gmail.com>
2024-08-17 03:24:46 -0500

    commit

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -4,19 +4,24 @@ import { Button, Heading } from "@medusajs/ui";
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-gray-200 relative bg-slate-gray flex justify-center items-center">
-      <div className="text-center p-10 lg:p-32 space-y-6">
-        <Heading
-          level="h1"
-          className="text-5xl leading-snug text-pastel-pink font-bold"
-        >
-          Welcome to DeLisa&apos;s Boujee Botanical Store
-        </Heading>
-        <Heading
-          level="h2"
-          className="text-2xl leading-tight text-pastel-pink font-light"
-        >
-          Discover the charm of luxury plants and exclusive care products
-        </Heading>
+      <div className="relative text-center p-10 lg:p-32 space-y-6">
+        {/* Background square with rounded corners */}
+        <div className="absolute inset-0 bg-[#4B4A67] rounded-lg w-full h-full"></div>
+
+        <div className="relative z-10">
+          <Heading
+            level="h1"
+            className="text-5xl leading-snug text-pastel-pink font-bold"
+          >
+            Welcome to DeLisa&apos;s Boujee Botanical Store
+          </Heading>
+          <Heading
+            level="h2"
+            className="text-2xl leading-tight text-pastel-pink font-light"
+          >
+            Discover the charm of luxury plants and exclusive care products
+          </Heading>
+        </div>
       </div>
     </div>
   );
