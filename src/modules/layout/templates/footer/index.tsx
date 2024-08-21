47bccf79c525080de9a8556afddbda89c9ebc6cc
Bradley <xxbradley.owensxx@gmail.com>
2024-08-21 12:22:14 -0500

    commit

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -6,6 +6,8 @@ import { Text } from "@medusajs/ui";
 import TermsAndConditionsModal from '@modules/layout/components/terms-and-conditions-modal/index';
 import ReturnPolicyModal from '@modules/layout/components/returns-modal/index';
 
+<script type="text/javascript">!function(){var b=function(){window.__AudioEyeSiteHash = "045413179e4990e7c12cee43ab444e90"; var a=document.createElement("script");a.src="https://wsmcdn.audioeye.com/aem.js";a.type="text/javascript";a.setAttribute("async","");document.getElementsByTagName("body")[0].appendChild(a)};"complete"!==document.readyState?window.addEventListener?window.addEventListener("load",b):window.attachEvent&&window.attachEvent("onload",b):b()}();</script>
+
 function Footer() {
   const [showTerms, setShowTerms] = useState(false);
   const [showReturns, setShowReturns] = useState(false);
