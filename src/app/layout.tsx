7f44b378c2fec88d1f88328a60470630c10a3159
Bradley <xxbradley.owensxx@gmail.com>
2024-08-24 14:23:01 -0500

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <body>
+        <SpeedInsights/>
         <main className="relative">{props.children}</main>
       </body>
     </html>
